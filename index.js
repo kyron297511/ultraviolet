d41a5ca5432038c9a00fe9fc372c3d0b9838ec76
kyron297511 <113534403+kyron297511@users.noreply.github.com>
2022-12-09 23:03:41 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,4 @@
+(async() => {
+  await import('./index.mjs');
+})();
+//If you want, you can fork this replit to have your own version of Ultraviolet. Make sure to follow me, and enjoy your free proxy!
